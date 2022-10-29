f5a8b519489c78e61d93b769ebc7c89f739f1a2a
NagarajanSanthosh <110588320+NagarajanSanthosh@users.noreply.github.com>
2022-10-29 22:14:54 +0530

    exchanging a and b values

--- fp.c
+++ fp.c
@@ -4,6 +4,10 @@ int main(void){
     a=1;
     b=2;
     c=a+b;
-    printf("%d",c);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("\n a = %d b = %d", a, b);
+    printf("\n%d",c);
     return 0;
 }
